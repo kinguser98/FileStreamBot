390961cf8020b6876f0250ef672807528e51818b
kinguser98 <121923367+kinguser98@users.noreply.github.com>
2024-05-17 15:16:45 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -28,11 +28,11 @@ class Telegram:
 
 class Server:
     PORT = int(env.get("PORT", 8080))
-    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "https://filestreambot-brw1.onrender.com/"))
+    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
-    FQDN = str(env.get("FQDN", BIND_ADDRESS))
+    FQDN = str(env.get("FQDN", 'https://filestreambot-brw1.onrender.com/'))
     URL = "http{}://{}{}/".format(
         "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
     )
